a656b1ee9226d053b172e7dda74d5a43b091af39
Fatima Rafiqui <fatima.rafiqui@gmail.com>
2016-03-08 21:21:12 +0530

    Create database_basic.py

---
+++ database_basic.py
@@ -0,0 +1,20 @@
+import MySQLdb
+
+db = MySQLdb.connect("localhost", "Student","","mona")
+curs = db.cursor()
+
+che = 1
+flag = 0
+while (che==1): 
+  f = raw_input ('Family member name\n')
+  print f
+  curs.execute("INSERT INTO mona(name)VALUES(%s)",(f))
+ db.commit()
+  flag = flag+1
+  che = input ("More Family Members (1/0)\n")
+  if (che==1):
+     print "Enter More members\n"
+  if (che==0):
+     break
+print flag 
+print "members of family\n "
